54d3a7711c39d9ea591ff6086b39467d88ef5023
doh <theshawns0n@gmail.com>
2020-10-01 21:51:44 -0400

    answers printing to template string in console.

--- README.md
+++ README.md
@@ -1,4 +1,39 @@
+
 # autoAuthorundefined
-undefined
-undefined
-undefined
+
+# TITLE
+
+Science
+
+## Description
+
+Its a science application
+
+## Installation
+
+Grab and Go
+
+## Usage
+
+Unload and Reload
+
+## Questions
+
+https://www.linkedin.com/in/shawn.yandall/
+https://github.com/dohmr/
+
+## Contributions or Limitations
+
+Shawn and Sean and Shaun
+
+
+## Licenses
+
+Three
+
+
+## Test
+
+Science
+
+    
--- index.js
+++ index.js
@@ -1,123 +1,133 @@
 const inquirer = require("inquirer");
-const fs = require ("fs");
-
+const fs = require("fs");
+const util = require("util");
+const writeFileSync = util.promisify(fs.writeFile);
 
 // function to write README file
 // function writeToFile(fileName, data) {
 // }
 
-//Append text after the last... (add , null, 4) for space on readME?
-fs.appendFile("README.md", process.argv[2] + '\n', function(err) {
-    if (err) {
-      console.log(err)
-      return console.log(err);
-    }  
 
-    // console.log("Anything else?");
-      });
 
-// function to initialize program
-function init() {
 
+// main();
+// // function to initialize program
+// function main() {
+//     promptUser()
+//     .then ((answers) => {
+//         const read = generateREAD(answers);
+//         return writeFileSync("README.md", read);
+        
+//     })
+//     .then(() => {
+//         console.log("READme has been written!");
+//     })
+//     .catch ((err) => {
+//         console.log(err);
+//     });
+// }
+
+
+promptUser();
+function promptUser() {
+    const questions = [
+        {
+            type: "input",
+            message: `What is the name of your Project?`,
+            name: "title",
+        },
+        {
+            type: "input",
+            message: `Please enter a description of your Application`,
+            name: "description",
+        },
+        {
+            type: "input",
+            message: `What are the instructions for installation of the Application?`,
+            name: "install",
+        },
+        {
+            type: "input",
+            message: `Please enter information pertaining to the usage of the Application? How it works?`,
+            name: "usage",
+        },
+        {
+            type: "input",
+            message: `Please list any contributors or limits to future contributing sources.`,
+            name: "limits",
+        },
+        {
+            type: "input",
+            message: `Please detail any Test instuctions for those using your code.`,
+            name: "test",
+        },
+        {
+            type: "checkbox",
+            message: "Please select any license('s) this Application is covered under.",
+            name: "licenses",
+            choices: [
+                "One",
+                "Two",
+                "Three",
+                "Four",
+            ]
+        },
+        {
+            type: "input",
+            message: "What is your Github username?",
+            name: "github",
+        },
+        {
+            type: "input",
+            message: "What is your LinkedIn username?",
+            name: "linked",
+        },
+                
+    ];
+    console.log(questions);
+    return inquirer.prompt(questions).then(answers => {
+        const read = generateREAD(answers);
+        console.log(read)});
 }
 
-// function call to initialize program
-// init();
-inquirer.prompt([
-    {
-        type: "input",
-        message:  `What is the name of your Project?`,
-        name: "title",
-    },
-    {
-        type: "input",
-        message: `Please enter a description of your Application`,
-        name: "description",
-    },
-    {
-        type: "input",
-        message: `What are the instructions for installation of the Application?`,
-        name: "install",
-    },
-    {
-        type: "input",
-        message: `Please enter information pertaining to the usage of the Application? How it works?`,
-        name: "usage",
-    },
-    {
-        type: "input",
-        message: `Please list any contributors or limits to future contributing sources.`,
-        name: "limits",
-    },
-    {
-        type: "input",
-        message: `Please detail any Test instuctions for those using your code.`,
-        name: "test",
-    },
-    {
-        type: "checkbox",
-        message: "Please select any license('s) this Application is covered under.",
-        name: "licenses",
-        choices: [
-            "One",
-            "Two",
-            "Three",
-            "Four",
-        ]
-    },
-    {
-        type: "input",
-        message: "What is your Github username?",
-        name: "github",
-    },
-    {
-        type: "input",
-        message: "What is your Github username?",
-        name: "linked",
-    },
-])
-
-function generateHTML(answers) {
+function generateREAD(answers) {
     return `
-  <!DOCTYPE html>
-  <html lang="en">
-    <head>
-      <meta charset="UTF-8" />
-      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-      <link
-        rel="stylesheet"
-        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-      />
-      <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-      />
-      <title>${answers.name}</title>
-    </head>
-    <body>
-      <div class="jumbotron jumbotron-fluid">
-        <div class="container">
-          <h1 class="display-4">Hi! My name is ${answers.name}</h1>
-          <p class="lead">I am from ${answers.location}.</p>
-          <a
-            class="btn btn-dark btn-lg"
-            role="button"
-            href="https://github.com/${answers.github}"
-            ><i class="fa fa-github"></i> GitHub</a
-          >
-          <a
-            class="btn btn-dark btn-lg"
-            role="button"
-            href="https://www.linkedin.com/in/${answers.linkedin}/"
-            ><i class="fa fa-linkedin-square"></i> LinkedIn</a
-          >
-        </div>
-      </div>
-      <div class="container">
-          <h2>About Me</h2>
-          <p><span class="font-weight-bold">Favorite Hobby:</span> ${answers.hobby}</p>
-          <p><span class="font-weight-bold">Favorite Food:</span> ${answers.food}</p>
-      </div>
-    </body>
-  </html>`;
-  }
+# autoAuthor
+
+# TITLE
+
+${answers.title}
+
+## Description
+
+${answers.description}
+
+## Installation
+
+${answers.install}
+
+## Usage
+
+${answers.usage}
+
+## Questions
+
+https://www.linkedin.com/in/${answers.linked}/
+https://github.com/${answers.github}/
+
+## Contributions or Limitations
+
+${answers.limits}
+
+
+## Licenses
+
+${answers.licenses}
+
+
+## Test
+
+${answers.test}
+`;
+
+}
